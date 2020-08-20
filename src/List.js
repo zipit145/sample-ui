e4e1080b51870a4416a5cc727746660ee0f8a42a
zipit145 <gregbforney@gmail.com>
2020-08-20 09:25:35 -0600

    pushing work prior to having to step away for a moment. React-bootstrap form has been added. Still deciding whether or not to use state in list component or move it all to the App.

--- src/List.js
+++ src/List.js
@@ -4,61 +4,99 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import Container from 'react-bootstrap/Container'
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 
 class List extends Component {
     
     constructor() {
         super();
         this.state = {
-            show: false
+            show: false,
+            valueName: '',
+            valueBody: ''
         }
-      }
+        this.handleChangeName = this.handleChangeName.bind(this);
+        this.handleChangeBody = this.handleChangeBody.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
     handleClose = () => this.setState({show: false});
     handleShow = () => this.setState({show: true});
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.valueName + this.state.valueBody)
+        this.setState({
+            show: false
+        });
+        event.preventDefault();
+    }
+    handleChangeName(event) {
+        this.setState({valueName: event.target.value});
+    }
+    handleChangeBody(event) {
+        this.setState({valueBody: event.target.value});
+    }
 
     render() {
-      return (
-        <div className="List">
-            <Container>
-                {this.props.listItems.map(item => {
-                    return(
-                        <Row>
-                            <Col sm={8}>{item.name}</Col>
-                            <Col sm={4}>{item.body}</Col>
-                        </Row>
-                    )
-                })}
-                <Row>                
-                    <Button variant="primary" onClick={this.handleShow}>
-                        Add Item
-                    </Button>
-                    <Button variant="primary" onClick={this.handleShow}>
-                        Delete Selected Item
-                    </Button>
-                </Row>
+        return (
+            <div className="List">
+                <Container>
+                    {this.props.listItems.map(item => {
+                        return(
+                            <Row>
+                                <Col sm={4}>{item.name}</Col>
+                                <Col sm={4}>{item.body}</Col>
+                            </Row>
+                        )
+                    })}
+                    <Row>
+                        <Col sm={4}>         
+                            <Button variant="primary" onClick={this.handleShow}>
+                                Add Item
+                            </Button>
+                        </Col> 
+                        <Col sm={4}>
+                            <Button variant="primary" onClick={this.handleShow}>
+                                Delete Selected Item
+                            </Button>
+                        </Col>
+                    </Row>
 
-            </Container>
-            <Modal
-                show={this.state.show}
-                onHide={this.handleClose}
-                keyboard={false}
-            >
-                <Modal.Header closeButton>
-                <Modal.Title>New List Item</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
+                </Container>
+                <Modal
+                    show={this.state.show}
+                    onHide={this.handleClose}
+                    keyboard={false}
+                >
+                    <Modal.Header closeButton>
+                    <Modal.Title>New List Item</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                    <Form  onSubmit={this.handleSubmit}>
+                        <Form.Group controlId="formBasicText">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Name" onChange={this.handleChangeName}/>
 
-                </Modal.Body>
-                <Modal.Footer>
-                <Button variant="secondary" onClick={this.handleClose}>
-                    Close
-                </Button>
-                <Button variant="primary">Understood</Button>
-                </Modal.Footer>
-            </Modal>
-        </div>
-      );
+                        </Form.Group>
+                        <Form.Group controlId="formBasicText">
+                            <Form.Label>Body</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Body" onChange={this.handleChangeBody}/>
+
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                    <Button variant="secondary" onClick={this.handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary">Understood</Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+        );
     }
   
 }
