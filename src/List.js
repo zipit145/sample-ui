b4163685720b532dfd3e78c864efbe563180c0fc
zipit145 <gregbforney@gmail.com>
2020-08-19 20:37:12 -0600

    added bootstrap modal and state to List component

--- src/List.js
+++ src/List.js
@@ -1,8 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal'
 
 class List extends Component {
+    
+    constructor() {
+        super();
+        this.state = {
+            show: false
+        }
+      }
+    handleClose = () => this.setState({show: false});
+    handleShow = () => this.setState({show: true});
+
     render() {
       return (
         <div className="List">
@@ -11,7 +22,31 @@ class List extends Component {
             <div>{item}</div>
             )
           })}
-          <Button variant="secondary" size="lg">Add Item</Button>
+
+            <Button variant="primary" onClick={this.handleShow}>
+                Add Item
+            </Button>
+
+            <Modal
+                show={this.state.show}
+                onHide={this.handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                <Modal.Title>Modal title</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                I will not close if you click outside me. Don't even try to press
+                escape key.
+                </Modal.Body>
+                <Modal.Footer>
+                <Button variant="secondary" onClick={this.handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary">Understood</Button>
+                </Modal.Footer>
+            </Modal>
         </div>
       );
     }
